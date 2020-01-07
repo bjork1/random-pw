2719f42b360b7f32eba154a293978e9d4c343dc4
Andrew Bjork <bjorkandrew1@gmail.com>
2020-01-07 10:20:48 -0600

    updating files

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-//Define array called characters with letters, numbers, and symbols
-//Join function to remove commas from output
+/*Define array called characters with letters, numbers, and symbols
+Join function to remove commas from output*/
 
 var characters = [
     "A",
@@ -64,7 +64,10 @@ var characters = [
     '7',
     '8',
     '9',
-    '0',
+    '0'
+  ].join('') ;
+
+  var symbols = [
     "!",
     "#",
     "$",
@@ -82,34 +85,71 @@ var characters = [
  ].join('') ;
 
 
+var array3 = characters.concat(symbols);
+
+
+console.log(array3);
+
+
+
 //Prompt to enter number of characters for password
+
+
+
+
 
 var digits = window.prompt("How many?");
 
 
-//Define function called randPassword(characters) to return number of characters entered in prompt
-//Create random combination of characters 
-//Join result to remove characters from output
+
+//Digits between 8 and 180 by if statements with while loop//
+
+while (digits < 7 || digits > 180 || isNaN(digits)) {
+    alert("Please try again");
+    var digits = window.prompt("How many?");
+
+}
+
+/*Define function called randPassword(characters) to return number of characters entered in prompt
+Create random combination of characters 
+Join result to remove characters from output*/
+
+var yes = confirm("Would you like to include symbols?");
+
+if (yes == true) {
+  var hello = array3;
+}
+else {
+  var hello = characters;
+}
 
 
-function randPassword(characters) {
+console.log(hello);
+
+
+function randPassword(hello) {
+
+
     
     var result =[];
     for (var i = 0; i < digits; i++){
-        result.push(characters[Math.floor(Math.random() * characters.length)]);
+        result.push(hello[Math.floor(Math.random() * hello.length)]);
     }
 
     return result.join('');
+  }
     
 
-  }
 
-//Define function genPassword to link click from HTML button
-//Link with ID "password" to return in text box of HTML
+/*Define function genPassword to link click from HTML button
+Link with ID "password" to return in text box of HTML*/
 
 function genPassword() {
-    document.getElementById("password").innerHTML = randPassword(characters);
+    document.getElementById("password").innerHTML = randPassword(hello);
 }
+
+
+//Copy to Clipboard function
 
 function copyToClipboard(element) {
   var $temp = $("<input>");
